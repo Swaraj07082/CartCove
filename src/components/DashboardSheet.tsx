fed414d3e3270f11f268c9c5ee11fd0e69e0beae
Swaraj <swarajmali072004@gmail.com>
2024-06-30 17:46:08 +0100

    product page

--- src/components/DashboardSheet.tsx
+++ src/components/DashboardSheet.tsx
@@ -65,7 +65,7 @@ export function DashboardSheet() {
             Orders
           </Link>
           <Link
-            href="#"
+            href="dashboard/product"
             className="flex items-center gap-3 rounded-lg  px-3 py-2 text-muted-foreground transition-all hover:text-primary"
           >
             <Package className="h-4 w-4" />
