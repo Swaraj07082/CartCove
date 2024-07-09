24dc803ad5870c3b0df1014083f98f325447f20b
Swaraj <swarajmali072004@gmail.com>
2024-07-09 21:09:08 +0100

    homepage add to cart

--- src/components/ui/Homepage.tsx
+++ src/components/ui/Homepage.tsx
@@ -1,10 +1,48 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import cameraimage from "../../../public/camera.jpg";
 import { Button } from "./button";
 import dummyproducts from "../../../public/Dummyshoes.json";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/app/redux/store";
+import {
+  addCartItem,
+  updateCartItemQuantity,
+} from "@/app/features/cart/cartSlice";
 
 export default function Homepage() {
+  const [quantity, setquantity] = useState<number>(1);
+
+  const cartItems = useSelector((state: RootState) => state.cart);
+  const Dispatch = useDispatch();
+
+  const addCartItemhandler = (
+    id: string,
+    name: string,
+    price: number,
+    quantity: number
+  ) => {
+    const existingCartItem = cartItems.find((item) => item.id === id);
+
+    if (existingCartItem) {
+      Dispatch(
+        updateCartItemQuantity({
+          id: id,
+          quantity: existingCartItem.quantity + quantity,
+        })
+      );
+    } else {
+      Dispatch(
+        addCartItem({
+          id: id,
+          name: name,
+          price: price,
+          quantity: quantity,
+        })
+      );
+    }
+  };
   return (
     <div className=" mx-24 flex flex-col gap-y-10 mt-10">
       <div className=" w-full h-64">
@@ -37,7 +75,21 @@ export default function Homepage() {
               </div>
               <div className=" text-xl font-semibold mt-1">{product.name}</div>
               <div className="text-xl  font-semibold">{product.price}</div>
-              <Button className=" w-full mt-1">Add to Cart</Button>
+              <Button
+                className=" w-full mt-1"
+                onClick={() => {
+                  // setquantity(quantity + 1);
+                  addCartItemhandler(
+                    String(product.id),
+                    product.name,
+                    product.price,
+                    quantity
+                  );
+                  // handleQuantityChange(product.id, quantity);
+                }}
+              >
+                Add to Cart
+              </Button>
             </div>
           ))}
         </div>
